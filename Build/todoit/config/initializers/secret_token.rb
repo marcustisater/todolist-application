eab775f754bc8e07258a9237fb45432c097f68e1
Marcus Tisäter <marcus.tisater@happiness.se>
2014-04-10 18:22:26 +0200

    added ruby on rails env

---
+++ Build/todoit/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todoit::Application.config.secret_key_base = 'df87266f25afd64976b3ad0e48af40baf52890e1c7c8dc7f8fee2692a053e14f900132f6153471bf418068a90d870d5a3a7e60d166d8df3e2e911b5babb7c91b'
---
+++ Build/todoit/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Todoit::Application.config.session_store :cookie_store, key: '_todoit_session'
