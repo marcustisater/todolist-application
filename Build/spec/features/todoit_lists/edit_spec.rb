e133b098dea9fa75a9088581ab92cedb03e708a3
Marcus Tisäter <marcus.tisater@happiness.se>
2014-05-03 10:59:03 +0200

    added DOM to add unique ids

---
+++ Build/spec/features/todoit_lists/edit_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'Editing todoit lists' do 
+
+  it "updates a todoit list succesfully with information that is correct"
+
+  visit "/todoit_lists"
+  
+
+ end 
+end
