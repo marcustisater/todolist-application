38be183c758a1f0eda693d8233f1599c902dbb7b
Marcus Tisäter <marcus.tisater@happiness.se>
2014-05-03 15:48:35 +0200

    tested the index

--- Build/app/controllers/todoit_items_controller.rb
+++ Build/app/controllers/todoit_items_controller.rb
@@ -1,4 +1,7 @@
 class TodoitItemsController < ApplicationController
   def index
+
+    @todoit_list = TodoitList.find(params[:todoit_list_id])
+
   end
 end
--- Build/spec/features/todoit_items/index_spec.rb
+++ Build/spec/features/todoit_items/index_spec.rb
@@ -3,16 +3,25 @@ require 'spec_helper'
 describe 'viewing todoit items' do 
   let!(:todoit_list) { TodoList.create(title: "Shopping", description: "buy carrots")}
 
-  it 'displays no items when a todoit list is empty ' do
+
+  before do 
 
   visit "/todoit_lists"
   within "#todoit_list_#{todo_list.id}" do
     click_link "List Items"
+   end 
   end 
 
-  expect(page).to have_content("TodoitItems#index")
 
-  
+  it 'displays the title of the todoit list' do
+    within("h1") do 
+      expect(page).to have_Contetn(todoit_list.title)
+  end 
+
+  it 'displays no items when a todoit list is empty ' do
+
+    expect(page.all("ul.todoit_items li").size).to eq(0)
+
   end 
 
 end
