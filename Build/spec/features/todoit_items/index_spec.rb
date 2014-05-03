88562cba0b8114eb7c43d3940c556a397caf98b3
Marcus Tisäter <marcus.tisater@happiness.se>
2014-05-03 15:55:46 +0200

    added more test to create Tasks

--- Build/spec/features/todoit_items/index_spec.rb
+++ Build/spec/features/todoit_items/index_spec.rb
@@ -4,16 +4,17 @@ describe 'viewing todoit items' do
   let!(:todoit_list) { TodoList.create(title: "Shopping", description: "buy carrots")}
 
 
-  before do 
+  def visit_todoit_list(list)
 
-  visit "/todoit_lists"
-  within "#todoit_list_#{todo_list.id}" do
+      visit "/todoit_lists"
+  within "#todoit_list_#{list.id}" do
     click_link "List Items"
    end 
-  end 
 
+  end
 
   it 'displays the title of the todoit list' do
+    visit_todoit_list(todoit_list)
     within("h1") do 
       expect(page).to have_Contetn(todoit_list.title)
   end 
@@ -23,5 +24,18 @@ describe 'viewing todoit items' do
     expect(page.all("ul.todoit_items li").size).to eq(0)
 
   end 
+
+  it "dispalys items contnt when a todoit list has tasks!"
+  todoit_list.todoit_items.create(content: "milk")
+  todoit_list.todoit_items.create(content: "milk2")
+  visit_todoit_list(todoit_list)
+  expect(page.all("ul.todoit_items li").size).to eq(2)
+  within "ul.todoit_items" do
+    todoit_list.todoit_items.create(content: "milk")
+    todoit_list.todoit_items.create(content: "milk2")
+  end 
+
+end 
+
 
 end
