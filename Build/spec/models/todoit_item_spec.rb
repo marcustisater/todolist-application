3888b929d415082222242cffb4f9237c35699969
Marcus Tisäter <marcus.tisater@happiness.se>
2014-05-11 13:08:57 +0200

    added module for completed mark

--- Build/spec/models/todoit_item_spec.rb
+++ Build/spec/models/todoit_item_spec.rb
@@ -2,4 +2,15 @@ require 'spec_helper'
 
 describe TodoitItem do
   it { should belong_to (:todoit_list)}
+
+
+  describe '#completed?' do
+    let(:todoit_item) { TodoitItem.create(content: "Hello")}
+
+    it "is false when completed_is blank" do
+      todoit_item.completed_at = nil
+      expect(todoit_item.completed?).to be_false
+    end 
+  end
+
 end
