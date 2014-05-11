47f63f37ba4269362a456958c854f7c766fcb26b
Marcus Tisäter <marcus.tisater@happiness.se>
2014-05-11 10:55:54 +0200

    fix tests

--- Build/app/controllers/todoit_items_controller.rb
+++ Build/app/controllers/todoit_items_controller.rb
@@ -13,6 +13,25 @@ class TodoitItemsController < ApplicationController
 
   def create 
 
-    
+    @todoit_list = TodoitList.find(params[:todoit_list_id])
+    @todoit_item = @todoit_list.todoit_items.new(todoit_item_params)
+
+    if @todoit_item.save
+      flash[:success] = "Added todoit list item"
+      redirect_to todoit_list_todoit_items_path
+    else
+      flash[:error] = "There was a problem adding that todoit list item"
+      render action: :new 
+   
+
+
+
+  end 
+
+
+  private 
+  def todoit_item_params 
+    params[:todoit_item].permit(:content)
+  end 
 
 end
