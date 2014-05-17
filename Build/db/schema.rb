aba334715dae90c5cf58eb4449964e081c9d0ac3
Marcus Tisäter <marcus.tisater@happiness.se>
2014-05-17 16:20:08 +0200

    migrate database

--- Build/db/schema.rb
+++ Build/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140511104449) do
+ActiveRecord::Schema.define(version: 20140517141751) do
 
   create_table "todoit_items", force: true do |t|
     t.integer  "todoit_list_id"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20140511104449) do
     t.string   "title"
     t.string   "description"
     t.string   "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
