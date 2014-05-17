d102d241fc54ca1079234bee04023188e485e2fc
Marcus Tisäter <marcus.tisater@happiness.se>
2014-05-17 16:29:40 +0200

    test for user

--- Build/spec/models/user_spec.rb
+++ Build/spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
-describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do 
+  let(:valid_attributes) {
+    first_name: "Marcus"
+    last_name: "Tisater"
+    email: "marcus.tisater@elev.it-gymnasiet.se"
+    }
+  }
+  context 'validations' do
+    let(user:) {
+      User.new(valid_attributes)
+    }
+    it "requires a user to have a emial address"
+    expect(user).to validate_presence_of_email 
+    end
+
+  end 
 end
