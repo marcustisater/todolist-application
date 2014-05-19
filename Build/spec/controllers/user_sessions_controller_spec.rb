da1c978971e22b7bd3e09227a977374364c2f9bd
Marcus Tisäter <marcus.tisater@happiness.se>
2014-05-19 14:09:19 +0200

    authentice

--- Build/spec/controllers/user_sessions_controller_spec.rb
+++ Build/spec/controllers/user_sessions_controller_spec.rb
@@ -7,13 +7,25 @@ describe UserSessionsController do
       get 'new'
       response.should be_success
     end
+
+    it "renders the new template" do
+      get 'new'
+      expect(response).to render_template('new')
+    end 
   end
 
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
-    end
+  describe "POST 'create'" do
+    it "ridrects to the todolist path" do
+      post :create, :email: "marcus.tisater@gmail.com", password: "hej12345"
+      expect(response).to be_redirect
+      expect(response).to redirect_to(todo_lists_path)
   end
 
+  it "authentiactes my user"
+      user = User.create(first_name: "Marcus", last_name: "Tisater", email: marcus.tisater@gmail.com, password: "hej12345", password_confirmation: "hej12345")
+      expect(user).to receive(:authentiactes)
+      post :create, :email: "marcus.tisater@gmail.com", password: "hej12345"
+      
+
+  end 
 end
