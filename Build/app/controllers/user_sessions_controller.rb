cfabaff2662de9dac7d4d9c6df76e930b63d46c1
Marcus Tisäter <marcus.tisater@happiness.se>
2014-05-19 14:22:26 +0200

    error if incorrect info

--- Build/app/controllers/user_sessions_controller.rb
+++ Build/app/controllers/user_sessions_controller.rb
@@ -4,7 +4,14 @@ class UserSessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
     redirect_to todo_lists_path
+
+  else
+    flash[:error] = "There was a problem logging into your ToDo account. Please check your password and e-mail otherwise contact Marcus!"
+    render action: 'new'
+    
+    end   
   end
 end
