f43e77b21d6094395600a9926f760fb656f45ee9
Marcus Tisäter <marcus.tisater@happiness.se>
2014-05-19 13:34:18 +0200

    better formating

--- Build/spec/controllers/users_controller_spec.rb
+++ Build/spec/controllers/users_controller_spec.rb
@@ -30,9 +30,7 @@ describe UsersController do
    "email" => "email@example.com",
    "password" => "password1245",
    "password_confirmation" => "password1245"
-   
-}
-}
+ }}
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
