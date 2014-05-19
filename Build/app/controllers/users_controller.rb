21d6734500e7f87588a2cacc681156a8549c5ded
Marcus Tisäter <marcus.tisater@happiness.se>
2014-05-19 15:03:06 +0200

    better formatting

--- Build/app/controllers/users_controller.rb
+++ Build/app/controllers/users_controller.rb
@@ -12,9 +12,10 @@ class UsersController < ApplicationController
 
   # POST /users
   # POST /users.json
+
   def create
     @user = User.new(user_params)
-
+ 
     respond_to do |format|
       if @user.save
         session[:user_id] = @user.id
